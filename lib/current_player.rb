a1a398522d843e4544dec3f174bd5f33fe7311c5
Kari O'Neal <triadfirewood@gmail.com>
2019-02-19 17:20:25 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,23 +1,25 @@
+require 'pry'
+
 def turn_count(board)
-  turn = 0
-  board.each do |p|
-    if p == "X" || p == "O"
+  turn = 0 
+  board.each do |position|
+    if position == "X" || position == "O"
       turn += 1
-   end
-  end    
-return turn
+      # binding.pry
+     end
+    end
+  turn
 end
     
 # require 'pry'
 def current_player(board)
-    if turn_count(board).even?
-      "X"
-    else
-      "O"
-      # binding.pry
+  if turn_count(board) % 2 == 0 
+    "X"
+  else
+    "O"
   end
 end
-      
+turn_count(board) % 2 == 0 ? "X" : "O"
       
       
 
